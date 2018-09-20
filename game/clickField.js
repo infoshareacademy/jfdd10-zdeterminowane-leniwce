651d1073de8d9497c5d7b7f732b6a8f87280205c
rmrotek <remek87@gmail.com>
2018-09-20 16:30:51 +0200

    comment to clickfield

--- game/clickField.js
+++ game/clickField.js
@@ -4,11 +4,10 @@
 var clickField = document.querySelector('.click-me')
 
 clickField.addEventListener('click', function () {
+    // animation
     clickField.classList.add('slide-out');
     setTimeout(function () {
         clickField.classList.remove('slide-out');
     }, 100)
-
-
 
 })
