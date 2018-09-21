94535015b6b448c660777f4eb754ac8cf46db9c6
KMDzik <katarzyna.dzik90@gmail.com>
2018-09-21 15:29:11 +0200

    click_me file deleted

--- game/clickField.js
+++ game/clickField.js
@@ -4,11 +4,11 @@
 var clickField = document.querySelector('.click-me')
 
 clickField.addEventListener('click', function () {
+    change_score();
     // animation
     clickField.classList.add('slide-out');
     setTimeout(function () {
         clickField.classList.remove('slide-out');
-        change_score(1);
     }, 100)
 
 })
