acdb7577613c95d7100e3a73a0efe5933265548f
Pawel <paweljozwiak1986@gmail.com>
2018-09-21 16:30:56 +0200

    count down popup begin

--- game/end.js
+++ game/end.js
@@ -4,10 +4,14 @@ var timeout = 30;
 var countClock = document.querySelector('.current-time');
 countClock.innerHTML = timeout;
 
+var popUp = document.querySelector('.pop-up')
+
 var interval = setInterval(function (){
   var countClock = document.querySelector('.current-time');
   countClock.innerHTML =(--timeout);
   if (timeout < 1 ){
     clearInterval(interval);
+    popUp.classList.add('.pop-up-continer-active')
   }
+
 },1000);
