46071710b9bf17fd8ee32d7f2ad0c280ff58f7aa
KMDzik <katarzyna.dzik90@gmail.com>
2018-09-25 17:10:32 +0200

    upgrate counter

--- game/upgrade.js
+++ game/upgrade.js
@@ -19,12 +19,13 @@ clickField.addEventListener('click', function () {
 
 })
 
+
+var score = parseInt(document.querySelector(".current-score").innerText);
 var upgrade = 0;
 var price = 20;
+var power = 10;
+var upgradeCount = 0;
 
-function clickUpgradeScore(modify) {
-    upgrade += modify;
-}
 
 var clickUpgradeField = document.querySelector(".click-upgrade.upgrade-item")
 
@@ -32,7 +33,35 @@ clickUpgradeField.addEventListener('click', function () {
     if (score < price) {
         return;
     }
-    clickUpgradeScore(4);
+    upgradeCount += 1;
+    upgrade += 4;
     change_score(-price);
     price += 20;
+
+    document.querySelector('.click-up-cost').innerText = price;
+    document.querySelector('.click-up-count').innerText = upgradeCount;
 })
+
+
+
+var automateOneField = document.querySelector('.auto-one');
+var upgradeField = document.querySelector('.click-upgrade');
+var upgradeOneTotal = 0;
+
+var upgradePower = 10;
+
+setInterval(function () {
+
+    var upgradePrice = parseInt(document.querySelector(".click-up-cost").innerHTML);
+
+    upgradePrice <= score ? upgradeField.classList.add('upgrade-available') : upgradeField.classList.remove('upgrade-available');
+
+    upgradeOneTotal = upgradePower * upgradeCount;
+    score += upgradeOneTotal;
+
+    document.querySelector(".current-score").innerHTML = score;
+    document.querySelector(".current-score-per-second").innerHTML = upgradeOneTotal + scoreOnClick;
+    scoreOnClick = 0;
+
+
+}, 1000)
